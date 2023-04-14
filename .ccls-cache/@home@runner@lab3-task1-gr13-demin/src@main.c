e922e3c19fc616c461bfe4e36f7e7b66d9abe8ad
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2023-04-14 04:57:11 +0000

    Did the task_1 in main.c

--- .ccls-cache/@home@runner@lab3-task1-gr13-demin/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr13-demin/src@main.c
@@ -1,8 +1,42 @@
-/* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+if (argc < 2) {
+printf("Usage: %s size\n", argv[0]);
+return 1;
+}
+int size = atoi(argv[1]);
+
+char str1[size + 1];
+char str2[size + 1];
+
+srand(time(NULL));
+
+printf("Input string 1:\n");
+for (int i = 0; i < size; i++) {
+    str1[i] = rand() % 26 + 'a';
+    printf("%c", str1[i]);
+}
+printf("\n");
+
+printf("Input string 2:\n");
+for (int i = 0; i < size; i++) {
+    str2[i] = rand() % 26 + 'a';
+    printf("%c", str2[i]);
+}
+printf("\n");
+
+char result[size * 2 + 1];
+int j = 0;
+for (int i = 0; i < size; i++) {
+    result[j++] = str1[i];
+    result[j++] = str2[i];
+}
+result[size * 2] = '\0';
+
+printf("Result:\n%s\n", result);
+
+return 0;
 }
